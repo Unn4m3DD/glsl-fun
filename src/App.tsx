b4690a209741a7232fab9ed94b64c78edc6f415a
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-06-14 11:10:43 +0100

    add react template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import "./App.css";
+
+export const App: FC = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      Hello World
+    </div>
+  );
+};
