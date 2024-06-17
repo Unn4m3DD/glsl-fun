0689601fb3a3c002215d2f76f4ea29c202229dd4
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-06-17 08:57:23 +0100

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -37,24 +37,10 @@ export const App = () => {
         <CardPage />
 
         <Environment preset="city" />
-        <Rig />
       </Defaults>
     </Canvas>
   );
 };
-
-function Rig() {
-  useFrame((state, delta) => {
-    easing.damp3(
-      state.camera.position,
-      [state.pointer.x * 2, state.pointer.y * 2, 18],
-      0.35,
-      delta
-    );
-    state.camera.lookAt(0, 0, -10);
-  });
-  return null;
-}
 
 const cardGeometry = new geometry.RoundedPlaneGeometry(1, 1, 0.025);
 
@@ -62,13 +48,7 @@ export function CardPage() {
   const {
     size: { width, height },
   } = useThree();
-  const openRef = useRef(false);
-  const translateY = useMemo(() => signal(-460), []);
-  const translateZ = useMemo(() => signal(0), []);
-  useFrame((_, delta) => {
-    easing.damp(translateY, "value", openRef.current ? 0 : -460, 0.2, delta);
-    easing.damp(translateZ, "value", openRef.current ? 200 : 0, 0.2, delta);
-  });
+
   return (
     <Root
       width={width}
@@ -82,52 +62,79 @@ export function CardPage() {
     >
       <Defaults>
         {shaders.map((shader, index) => (
-          <Container
-            key={index}
-            backgroundColor={0xffffff}
-            dark={{ backgroundColor: 0x0 }}
-            borderRadius={20}
-            onClick={(e) => (
-              e.stopPropagation(), (openRef.current = !openRef.current)
-            )}
-            cursor="pointer"
-            flexDirection="column"
-            width={400}
-            zIndexOffset={10}
-            transformTranslateZ={translateZ}
-          >
-            <Content
-              transformTranslateZ={1}
-              padding={14}
-              keepAspectRatio={false}
-              width={400}
-              height={400}
-            >
-              <FragmentShaderRenderer
-                shader={shader}
-                cardGeometry={cardGeometry}
-              />
-            </Content>
-            <Container
-              backgroundColor={0xffffff}
-              dark={{ backgroundColor: 0x0 }}
-              flexDirection="row"
-              padding={28}
-              paddingTop={28 + 4}
-              alignItems="center"
-              justifyContent="space-between"
-              borderBottomRadius={20}
-              castShadow
-            >
-              <Container flexDirection="column" gap={8}>
-                <Text fontWeight="normal" fontSize={24} lineHeight="100%">
-                  Shader {`${index + 1}`}
-                </Text>
-              </Container>
-            </Container>
-          </Container>
+          <ShaderCard key={`${index}`} shader={shader} index={index} />
         ))}
       </Defaults>
     </Root>
+  );
+}
+
+function ShaderCard({
+  key,
+  shader,
+  index,
+}: {
+  key: string;
+  shader: string;
+  index: number;
+}) {
+  const openRef = useRef(false);
+  const translateZ = useMemo(() => signal(0), []);
+  const rotateX = useMemo(() => signal(0), []);
+  useFrame((_, delta) => {
+    // easing.damp3(
+    //   openRef.current.,
+    //   [state.pointer.x * 2, state.pointer.y * 2, 18],
+    //   0.35,
+    //   delta
+    // );
+    // easing.damp(translateY, "value", openRef.current ? 0 : -460, 0.2, delta);
+    
+    easing.damp(rotateX, "value", openRef.current ? 360: 0, 0.2, delta);
+    easing.damp(translateZ, "value", openRef.current ? 1100 : 0, 0.2, delta);
+  });
+  return (
+    <Container
+      key={key}
+      backgroundColor={0xffffff}
+      dark={{ backgroundColor: 0x0 }}
+      borderRadius={20}
+      onClick={(e) => (
+        e.stopPropagation(), (openRef.current = !openRef.current)
+      )}
+      cursor="pointer"
+      flexDirection="column"
+      width={400}
+      zIndexOffset={10}
+      transformTranslateZ={translateZ}
+      transformRotateY={rotateX}
+    >
+      <Content
+        transformTranslateZ={1}
+        padding={14}
+        keepAspectRatio={false}
+        width={400}
+        height={400}
+      >
+        <FragmentShaderRenderer shader={shader} cardGeometry={cardGeometry} />
+      </Content>
+      <Container
+        backgroundColor={0xffffff}
+        dark={{ backgroundColor: 0x0 }}
+        flexDirection="row"
+        padding={28}
+        paddingTop={28 + 4}
+        alignItems="center"
+        justifyContent="space-between"
+        borderBottomRadius={20}
+        castShadow
+      >
+        <Container flexDirection="column" gap={8}>
+          <Text fontWeight="normal" fontSize={24} lineHeight="100%">
+            Shader {`${index + 1}`}
+          </Text>
+        </Container>
+      </Container>
+    </Container>
   );
 }
